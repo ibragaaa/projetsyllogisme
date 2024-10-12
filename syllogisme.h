8cbf703e6947da51df54eacbb71e75a586187c65
Theo-username <theoalbtanv@gmail.com>
2024-10-12 17:50:04 +0200

    Create syllogisme.h

---
+++ syllogisme.h
@@ -0,0 +1,14 @@
+#ifndef SYLLOGISME_H
+#define SYLLOGISME_H
+
+void askSylo();
+int askOrderSeizure();
+void askQuantificateur();
+void printList(char **list, int size);
+void methode1();
+void methode2();
+void scanfList(char **list, int size);
+void scanfNom(char *nom);
+void scanfTypeFigure();
+
+#endif
