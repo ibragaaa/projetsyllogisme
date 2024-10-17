d2910c2bfb16b90d38af52ce5eb6ff7f8decce94
Theo-username <theoalbtanv@gmail.com>
2024-10-17 19:16:30 +0200

    Update syllogisme.c

--- syllogisme.c
+++ syllogisme.c
@@ -42,7 +42,11 @@ int size_quant_existentielles_negatif = 2;
 char *list_quant_existentielles_negatif[] = {
     "Pas tout le monde",
     "Pas toujours"};
-
+/*Fonction qui prend en param un Syllogisme , elle demande a l'utilisateur qu'elle methode utiliser pour saisir un syllogisme
+- 0 pour le rien faire
+- 1 pour utiliser la methode 1
+- 2 pour utiliser la methode 2
+*/
 void askSylo(Syllogysme *s)
 {
     printf("Bonjour \nNous allons commencer la saisie d'un Syllogisme !! \n");
@@ -63,7 +67,28 @@ void askSylo(Syllogysme *s)
         printf("saisie ANNULE\n");
     }
 }
-
+/*fonction fais parti de la fonction askSylo pour la demande de saisie d'un syllogisme*/
+int askOrderSeizure()
+{
+    printf("Quel est l'ordre de saisie du syllogisme choisi , Ordre : 1 avec 7 demandes ou 2 avec 9 demandes mais dans l'ordre de lecture du syllogisme\n");
+    printf("Veuillez entrer le nombre entier 1 ou 2 ou 0 pour annule la saisie\n");
+    int ordre = -1;
+    int res = scanf("%d", &ordre);
+    if (res == 1 && (ordre == 1 || ordre == 2 || ordre == 0))
+    {
+        return ordre;
+    }
+    else if (res != 1)
+    {
+        printf("Erreur  ");
+    }
+    else
+    {
+        printf("La saisie n'est pas VALIDE\n");
+        return askOrderSeizure();
+    }
+}
+/*fonction qui initialise un Syllogisme */
 Syllogysme initSyllo()
 {
     Syllogysme s;
@@ -76,12 +101,15 @@ Syllogysme initSyllo()
     s.S = true;
     s.P = true;
     s.figure = 0;
+    strcpy(s.quant1, "");
+    strcpy(s.quant2, "");
+    strcpy(s.quant3, "");
+    strcpy(s.sujet, "");
     strcpy(s.predicat, "");
     strcpy(s.moyenTerme, "");
-    strcpy(s.sujet, "");
     return s;
 }
-
+/* fonction de saisie pour la methode 1*/
 void methode1(Syllogysme *s)
 {
     printf("Etape (1 / 7) :\n");
@@ -95,22 +123,158 @@ void methode1(Syllogysme *s)
     askQuantificateur(s, 3);
     printf("Etape (4 / 7) :\n");
     printf("sujet\n");
-    scanfNom("sujet", s, 1);
+    strcpy(s->sujet, scanfNom("sujet"));
     printf("Etape (5 / 7) :\n");
     printf("predicat\n");
-    scanfNom("predicat", s, 2);
+    strcpy(s->predicat, scanfNom("predicat"));
     printf("Etape (6 / 7) :\n");
     printf("moyen terme\n");
-    scanfNom("moyen terme", s, 3);
+    strcpy(s->moyenTerme, scanfNom("moyen terme"));
     printf("Etape (7 / 7) :\n");
     printf("Type de la figure\n");
     scanfTypeFigure(s);
     printSyll(s);
 }
+/* fonction de saisie pour la methode 2*/
 void methode2(Syllogysme *s)
 {
+    printf("Etape (1 / 9) :\n");
+    printf("1er quant\n");
+    askQuantificateur(s, 1);
+    printf("Etape (2 / 9) :\n");
+    char sujet1[50];
+    strcpy(sujet1, scanfNom("sujet"));
+    printf("Etape (3 / 9) :\n");
+    char predicat1[50];
+    strcpy(predicat1, scanfNom("predicat"));
+    printf("Etape (4 / 9) :\n");
+    printf("2eme quant\n");
+    askQuantificateur(s, 2);
+    printf("Etape (5 / 9) :\n");
+    char sujet2[50];
+
+    strcpy(sujet2, demande1(sujet1, predicat1, s));
+    if (strcmp(sujet2, "") == 0)
+    {
+        printf("ERREUR");
+    }
+    else
+    {
+        if (strcmp(sujet2, sujet1) == 0)
+        {
+            strcpy(s->moyenTerme, sujet1);
+            strcpy(s->predicat, predicat1);
+        }
+        else if (strcmp(sujet2, predicat1) == 0)
+        {
+            strcpy(s->predicat, sujet1);
+            strcpy(s->moyenTerme, predicat1);
+        }
+        else
+        {
+            strcpy(s->sujet, sujet2);
+        }
+    }
+    printf("Etape (6 / 9) :\n");
+    if (strcmp(s->sujet, "") == 0)
+    {
+        strcpy(s->sujet, scanfNom("predicat de la 2eme proposition"));
+    }
+    else
+    {
+        printf("Il n'y a pas d'etape 6 dans se cas\n");
+    }
+
+    printf("Etape (7 / 9) :\n");
+    printf("quantificateur de la conclusion\n");
+    askQuantificateur(s, 3);
+    printf("Etape (8 / 9) :\n");
+    demande2(s);
+    printf("Etape (9 / 9) :\n");
+
+    printSyll(s);
+}
+/*focntion qui demande a l'utilisateur si le sujet de la deuxime
+proposition est le sujet ou le predicat de la premiere
+si oui pas de saisie de */
+char *demande1(char *s1, char *s2, Syllogysme *s)
+{
+    printf("le sujet de la deuxieme premisse est : \"%s\" , \"%s\" ou autre :\n", s1, s2);
+    printf("- 0 pour annule la saisie\n");
+    printf("- 1 pour %s\n", s1);
+    printf("- 2 pour %s\n", s2);
+    printf("- 3 pour la saisie du sujet\n");
+    int a;
+    int ret = scanf("%d", &a);
+    char *res;
+    strcpy(res, "");
+    if (ret != 1)
+    {
+        printf("Erreur\n");
+    }
+    else
+    {
+        if (a == 0)
+        {
+            printf("FIN DE LA SAISIE\n");
+        }
+        else if (a == 1)
+        {
+            s->figure = 3;
+            s->S = false;
+            s->P = true;
+            strcpy(res, s1);
+        }
+        else if (a == 2)
+        {
+            s->figure = 4;
+            s->S = false;
+            s->P = false;
+            strcpy(res, s2);
+        }
+        else if (a == 3)
+        {
+            printf("fais\n");
+            strcpy(res, scanfNom("sujet"));
+        }
+        else
+        {
+            printf("La saisie n'est pas VALIDE\n");
+            demande1(s1, s2, s);
+        }
+    }
+    return res;
 }
 
+void demande2(Syllogysme *s)
+{
+    printf("le sujet de la conclusion est le sujet de la 2eme premisse ou le predicat de la 2eme premisse \n");
+    printf("- 0 pour annule la saisie\n");
+    printf("- 1 le sujet est sujet dans la 2eme premisse\n");
+    printf("- 2 le sujet est le predicat de la 2eme premisse\n");
+    int a;
+    int ret = scanf("%d", &a);
+    if (ret != 1)
+    {
+        printf("Erreur\n");
+    }
+    else
+    {
+        if (a == 1)
+        {
+        }
+        else if (a == 2)
+        {
+        }
+        else
+        {
+            printf("La saisie n'est pas VALIDE\n");
+            demande2(s);
+        }
+    }
+}
+
+/*Fonction qui affiche un syllogisme en fonction du type de figure */
 void printSyll(Syllogysme *s)
 {
     printf("Affichage du Syllogisme :\n");
@@ -130,49 +294,38 @@ void printSyll(Syllogysme *s)
     {
         printf("Figure : %d\n%s %s %s , %s %s %s , %s %s %s\n", s->figure, s->quant1, s->predicat, s->moyenTerme, s->quant2, s->moyenTerme, s->sujet, s->quant3, s->sujet, s->predicat);
     }
+    printf("U1 : %d ,U2 : %d ,Uc : %d ,A1 : %d ,A2 : %d ,Ac : %d ,S : %d ,P : %d\n", s->U1, s->U2, s->Uc, s->A1, s->A2, s->Ac, s->S, s->P);
 }
 
-int askOrderSeizure()
-{
-    printf("Quel est l'ordre de saisie du syllogisme choisi , Ordre : 1 avec 7 demandes ou 2 avec 9 demandes mais dans l'ordre de lecture du syllogisme\n");
-    printf("Veuillez entrer le nombre entier 1 ou 2 ou 0 pour annule la saisie\n");
-    int ordre = -1;
-    int res = scanf("%d", &ordre);
-    if (res == 1 && (ordre == 1 || ordre == 2 || ordre == 0))
-    {
-        return ordre;
-    }
-    else if (res != 1)
-    {
-        printf("Erreur  ");
-    }
-    else
-    {
-        printf("La saisie n'est pas VALIDE\n");
-        askOrderSeizure();
-    }
-}
-
-void scanfNom(char *nom, Syllogysme *s, int val)
+/*fonction qui prend en param le nom : soit le sujet , soit le predicat , soit le moyen terme ( juste pour l'affichage)
+et un entier val avec trois valeur possible 1 , 2 ou 3 respectivement sujet , predicat , moyen terme */
+char *scanfNom(char *nom)
 {
     printf("Donner le %s du syllogisme , Attention 50 caractere MAX sinon ERREUR\n", nom);
-    char sujet[50];
-    scanf("%s", &sujet);
+    printf("!!!!!!!!!!!! <  #  > pour arrete la saisie !!!!!!!!!!!!!!!\n");
+    char *sujet;
+    strcpy(sujet, "");
+    bool sorti = true;
+    while (sorti)
+    {
+        char compare[50];
+        scanf("%s", &compare);
+        if (strcmp(compare, "#") == 0)
+        {
+            sorti = false;
+        }
+        else
+        {
+            strcat(sujet, compare);
+            strcat(sujet, " ");
+        }
+    }
     printf("le %s : %s\n", nom, sujet);
-    if (val == 1)
-    {
-        strcpy(s->sujet, sujet);
-    }
-    else if (val == 2)
-    {
-        strcpy(s->predicat, sujet);
-    }
-    else if (val == 3)
-    {
-        strcpy(s->moyenTerme, sujet);
-    }
+    return sujet;
 }
-
+/*fonction qui fais la demande du type de figure choisi
+- 0 pour annule la saisie
+- 1,2,3,4 pour la figure choisie*/
 void scanfTypeFigure(Syllogysme *s)
 {
     printf(" ------------       ------------       ------------       ------------ \n");
@@ -210,6 +363,10 @@ void scanfTypeFigure(Syllogysme *s)
             s->P = false;
             s->S = false;
         }
+        else if (val == 0)
+        {
+            printf("Saisie ANNULE");
+        }
     }
     else if (res != 1)
     {
@@ -221,7 +378,13 @@ void scanfTypeFigure(Syllogysme *s)
         scanfTypeFigure(s);
     }
 }
-
+/*fonction qui demande a l'utilisateur quel type de quntificateur il choisi
+- 0 pour annule la saisie
+- 1 quantificateur existencielles
+- 2 quantificateur universelles
+- 3 quantificateur universelles negatif
+- 4 quantificateur existencielles negatif
+*/
 void askQuantificateur(Syllogysme *s, int valQuant)
 {
     printf("Quel type de quantificateur : 1 existentielle , 2 universelle , 3 universelle negatif , 4 existentielle negatif ou 0 pour annule la saisie \n");
@@ -300,7 +463,7 @@ void askQuantificateur(Syllogysme *s, int valQuant)
         printf("Erreur");
     }
 }
-
+/*fonction qui revoie une chaine de carractere choisi en fonction d'un liste donner et de sa taille en demandant un chiffre valide a l'utilisateur  */
 char *scanfList(char **list, int size)
 {
     printf("Entre un chiffre entre O et %d\n", size - 1);
@@ -318,10 +481,10 @@ char *scanfList(char **list, int size)
     else
     {
         printf("La saisie n'est pas VALIDE\n");
-        scanfList(list, size);
+        return scanfList(list, size);
     }
 }
-
+/*fonction qui affiche une liste donner en param avec la taille de la liste */
 void printList(char **list, int size)
 {
     for (int i = 0; i < size; i++)
