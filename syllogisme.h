47e9aedf882dd0ed318e1b1876acea247dc35bfd
Theo-username <theoalbtanv@gmail.com>
2024-10-21 18:45:50 +0200

    Update syllogisme.h

--- syllogisme.h
+++ syllogisme.h
@@ -1,30 +1,35 @@
 #ifndef SYLLOGISME_H
 #define SYLLOGISME_H
 
+#include "listQuant.h"
 #include <stdbool.h>
 
 typedef struct
 {
     bool U1, U2, Uc, A1, A2, Ac, S, P;
-    char quant1[50], quant2[50], quant3[50], sujet[50], predicat[50], moyenTerme[50];
+    char *quant1, *quant2, *quant3, *sujet, *predicat, *moyenTerme;
     int figure;
-} Syllogysme;
+} syllogisme;
 
-void askSylo(Syllogysme *s);
-Syllogysme *initSyllo();
-void freeSyllo(Syllogysme *s);
-int askOrderSeizure();
-void askQuantificateur(Syllogysme *s, int valQuant);
-void printList(char **list, int size);
-void methode1(Syllogysme *s);
-void methode2(Syllogysme *s);
-char *scanfList(char **list, int size);
-char *scanfNom(char *nom);
-void scanfTypeFigure(Syllogysme *s);
-void printSyll(Syllogysme *s);
+syllogisme *initSyllo();
+void freeSyllo(syllogisme *s);
+void printSyllo(syllogisme *s);
 
-char *demande1(char *s1, char *s2, Syllogysme *s);
-void demande1_bis(char *s1, char *s2, Syllogysme *s);
-void demande2(Syllogysme *s);
+int askInt(int max);
+char *askString();
+
+void changeList(quantificateurs *a, quantificateurs *b, quantificateurs *c, quantificateurs *d);
+
+void saisie(quantificateurs *a, quantificateurs *b, quantificateurs *c, quantificateurs *d, syllogisme *s);
+
+void mode1(quantificateurs *a, quantificateurs *b, quantificateurs *c, quantificateurs *d, syllogisme *s);
+void mode2(quantificateurs *a, quantificateurs *b, quantificateurs *c, quantificateurs *d, syllogisme *s);
+bool mode2_1(syllogisme *s);
+void mode2_2(syllogisme *s);
+void mode2_3(syllogisme *s);
+void detSyllo(char *s1, char *p1, syllogisme *s);
+void askQuant(quantificateurs *a, quantificateurs *b, quantificateurs *c, quantificateurs *d, syllogisme *s, int num);
+char *getListQuant(quantificateurs *list);
+void scanfTypeFigure(syllogisme *s);
 
 #endif
